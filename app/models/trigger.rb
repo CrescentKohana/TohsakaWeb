cb2c29ee10468430f56755f8e6efdbdbe9119988
Luukuton <mleinikka@gmail.com>
2020-05-29 06:47:21 +0300

    Overall adjustments

--- app/models/trigger.rb
+++ app/models/trigger.rb
@@ -2,13 +2,15 @@ class Trigger < ApplicationRecord
   validates :phrase, presence: true
   validates :mode, presence: true
   validates :server_id, presence: true
-  #validate :reply_xor_file
+
+  validate :reply_xor_file
 
   mount_uploader :file, FileUploader
+  validates :file, file_size: { less_than: 8.megabytes }
 
   private
   def reply_xor_file
-    unless reply.blank? ^ file.blank?
+    unless reply.blank? ^ file.filename.nil?
       errors.add(:base, "Specify a reply or a file, not both")
     end
   end
