ecc5c7c623c0be87eab7776f5f849ea5dbf36eca
Luukuton <mleinikka@gmail.com>
2020-06-03 01:38:30 +0300

    Quick fix for adding reminders without repeat

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -10,8 +10,8 @@ class Reminder < ApplicationRecord
   private
   def repeat_interval_as_seconds
     seconds = (self.repeat_min.to_i * 60) + (self.repeat_hour.to_i * 60 * 60) + (self.repeat_day.to_i * 24 * 60 * 60)
-    if seconds < 43200
-      errors.add(:base, "Interval has to be 12 hours or more.")
+    unless seconds == 0
+      errors.add(:base, "Interval has to be 12 hours or more.") if seconds < 43200
     end
   end
 end
