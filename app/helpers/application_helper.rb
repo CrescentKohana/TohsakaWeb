3d2c364e98964de6076b4abf0e9169ea9329c6ef
Luukuton <mleinikka@gmail.com>
2020-06-09 23:23:03 +0300

    Refactored CSS and added initial support for themes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def current_theme
+    session[:style] || 'light'
+  end
+
+  def switch_theme
+    if current_theme == 'light'
+      session[:style] = 'dark'
+    else
+      session[:style] = 'light'
+    end
+  end
 end
