3edd1f187f8268efb4993118291b961eb7564c93
Luukuton <mleinikka@gmail.com>
2021-03-08 16:08:27 +0200

    Resolved a possible CRF by allowing only POST as a registration/login method

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'sessions/create'
   get 'sessions/failure'
 
-  get '/login', :to => 'sessions#new', :as => :login
+  post '/login', :to => 'sessions#new', :as => :login
   get '/logout', :to => 'sessions#destroy', :as => :logout
 
   match '/auth/:provider/callback', :to => 'sessions#create', :via => [:get, :post]
