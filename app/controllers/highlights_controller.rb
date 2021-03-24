b166d21c163013d586f3bb2c50947a896f499469
Luukuton <mleinikka@gmail.com>
2021-03-24 19:02:21 +0200

    Added Show details page and attachment previews for Highlights

--- app/controllers/highlights_controller.rb
+++ app/controllers/highlights_controller.rb
@@ -10,4 +10,30 @@ class HighlightsController < ApplicationController
     return unless redirect_if_anonymous
     @highlight = Highlight.find(params[:id])
   end
+
+  private
+  def attachments_as_array(attachments)
+    attachments.split
+  end
+
+  def attachment_preview(attachment)
+    if %w[.jpg .png .jpeg .JPG .PNG .JPEG .gif].include?(File.extname(attachment))
+      html = %{<img src="#{attachment}" alt="image attachment" width=400>}
+    elsif %w[.mp4 .webm .mov].include?(File.extname(attachment))
+      html = '<video controls width="400">'\
+             "<source src=\"#{attachment}\" type=\"video/#{File.extname(attachment)[1..]}\">"\
+             'Sorry, your browser does not support embedded videos.'\
+             '</video>'
+    elsif  %w[.wav .flac .ogg .mp3].include?(File.extname(attachment))
+      html = "<audio controls src=\"#{attachment}\">"\
+             'Your browser does not support the audio element.'\
+             '</audio>'
+    else
+      html = "<p>Attachment cannot be previewed.</p>"
+    end
+
+    html.html_safe
+  end
+
+  helper_method :attachments_as_array, :attachment_preview
 end
